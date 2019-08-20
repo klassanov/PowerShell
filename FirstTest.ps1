9d740288625d9251f7d37c1fac0901e40d00183a
Alexander Klassanov <klassanov@gmail.com>
2019-08-20 18:38:32 +0300

    Logic added

--- FirstTest.ps1
+++ FirstTest.ps1
@@ -1,4 +1,21 @@
-﻿$wd = New-Object -ComObject Word.Application
+﻿#Add-Type -AssemblyName Microsoft.Office.Interop.Excel
+
+function Get-CellValue {
+ param($table, $x, $y )
+ 
+ $x+$y
+ 
+}
+
+
+
+Get-CellValue null 3 4
+
+
+
+
+
+$wd = New-Object -ComObject Word.Application
 $wd.Visible = $false
 
 Get-ChildItem -Filter *.doc | Foreach-Object {
@@ -6,6 +23,9 @@ Get-ChildItem -Filter *.doc | Foreach-Object {
         $filename= $_.Name
         #$filename="E:\FAKTURI 2019\F737.doc"
         $doc = $wd.Documents.Open($filename, $false, $true)
+
+        
+
         $table1= $doc.Tables.Item(1)
 
         $description = $table1.Cell(13,2).Range.Text
