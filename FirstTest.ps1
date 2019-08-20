85dbb9d0082767142c4d9f594aa78e7fdccf172f
klassanov@gmail.com <klassanov@gmail.com>
2019-08-20 19:18:28 +0300

    Tested loop through files

--- FirstTest.ps1
+++ FirstTest.ps1
@@ -9,10 +9,7 @@ function Get-CellValue {
 
 
 
-Get-CellValue null 3 4
-
-
-
+#Get-CellValue null 3 4
 
 
 $wd = New-Object -ComObject Word.Application
@@ -20,23 +17,16 @@ $wd.Visible = $false
 
 Get-ChildItem -Filter *.doc | Foreach-Object {
     
-        $filename= $_.Name
-        #$filename="E:\FAKTURI 2019\F737.doc"
-        $doc = $wd.Documents.Open($filename, $false, $true)
-
-        
-
+        $fullFilename= $_.FullName
+        $doc = $wd.Documents.Open($fullFilename, $false, $true)
         $table1= $doc.Tables.Item(1)
-
         $description = $table1.Cell(13,2).Range.Text
         $value = $table1.Cell(13,6).Range.Text
         $data=$table1.Cell(10,2).Range.Text
         $number=$table1.Cell(9,2).Range.Text
 
-        $description
-        $value
-        $data
-        $number
+        $_.Name +": "+$description +"|"+$value+"|"+$data+ "|"+$number
+        
 }
 
 
