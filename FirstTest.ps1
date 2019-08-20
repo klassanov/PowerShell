50fba88f5c97ff447865b34f286158a2ae038f3e
klassanov@gmail.com <klassanov@gmail.com>
2019-08-20 19:24:48 +0300

    Function Get-Cell value usage

--- FirstTest.ps1
+++ FirstTest.ps1
@@ -1,18 +1,4 @@
-﻿#Add-Type -AssemblyName Microsoft.Office.Interop.Excel
-
-function Get-CellValue {
- param($table, $x, $y )
- 
- $x+$y
- 
-}
-
-
-
-#Get-CellValue null 3 4
-
-
-$wd = New-Object -ComObject Word.Application
+﻿$wd = New-Object -ComObject Word.Application
 $wd.Visible = $false
 
 Get-ChildItem -Filter *.doc | Foreach-Object {
@@ -20,13 +6,27 @@ Get-ChildItem -Filter *.doc | Foreach-Object {
         $fullFilename= $_.FullName
         $doc = $wd.Documents.Open($fullFilename, $false, $true)
         $table1= $doc.Tables.Item(1)
-        $description = $table1.Cell(13,2).Range.Text
-        $value = $table1.Cell(13,6).Range.Text
-        $data=$table1.Cell(10,2).Range.Text
-        $number=$table1.Cell(9,2).Range.Text
+
+        #$description = $table1.Cell(13,2).Range.Text
+        $description = Get-CellValue $table1 13 2
+        
+        #$value = $table1.Cell(13,6).Range.Text
+        $value = Get-CellValue $table1 13 6
+
+        #$data=$table1.Cell(10,2).Range.Text
+        $data= Get-CellValue $table1 10 2
+
+        #$number=$table1.Cell(9,2).Range.Text
+        $number=Get-CellValue $table1 9 2
 
         $_.Name +": "+$description +"|"+$value+"|"+$data+ "|"+$number
         
+}
+
+
+function Get-CellValue {
+ param($table, $x, $y )
+    $table1.Cell($x,$y).Range.Text
 }
 
 
